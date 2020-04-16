2a87f0be63c342f88523d3dc07b1be08a437fab7
Bruno Pimentel <bruno@pimentel.dev>
2020-04-15 23:56:03 -0300

    ci: starts changing jacoco script
    - starts changing jacoco script due to pure kotlin modules besides the main
    android module

---
+++ domain/src/test/java/dev/pimentel/domain/UnitTest.kt
@@ -0,0 +1,12 @@
+package dev.pimentel.domain
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class UnitTest {
+
+    @Test
+    fun `should return sum`() {
+        assertEquals(2 + 2, 4)
+    }
+}
