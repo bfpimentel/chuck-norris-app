da7aef8bf7adc8557ffd54444ff81dbfada2629b
Bruno Pimentel <bruno@pimentel.dev>
2020-04-16 19:29:39 -0300

    feat: adds simple contract and fragment for feed

---
+++ app/src/main/java/dev/pimentel/chucknorris/facts/FactsContract.kt
@@ -0,0 +1,8 @@
+package dev.pimentel.chucknorris.facts
+
+import dev.pimentel.chucknorris.shared.abstractions.BaseContract
+
+interface FactsContract {
+
+    interface ViewModel : BaseContract.ViewModel
+}
---
+++ app/src/main/java/dev/pimentel/chucknorris/facts/FactsFragment.kt
@@ -0,0 +1,6 @@
+package dev.pimentel.chucknorris.facts
+
+import dev.pimentel.chucknorris.shared.abstractions.BaseFragment
+
+class FactsFragment: BaseFragment<> {
+}
