4d5421f28c2bf31a2533cf51cd1ef970697a3e34
Bruno Pimentel <bruno@pimentel.dev>
2020-04-16 17:58:08 -0300

    feat: adds retrofit dependency and the API url
    - adds retrofit singleton to DataModule
    - changes all modules to android libraries

---
+++ data/src/main/java/dev/pimentel/data/DataModule.kt
@@ -0,0 +1,39 @@
+package dev.pimentel.data
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+private const val REQUEST_TIMEOUT = 60L
+
+private val networkModule = module {
+    single {
+        val apiUrl = androidContext().getString(R.string.news_api_url)
+
+        OkHttpClient.Builder()
+            .readTimeout(REQUEST_TIMEOUT, TimeUnit.SECONDS)
+            .writeTimeout(REQUEST_TIMEOUT, TimeUnit.SECONDS)
+            .connectTimeout(REQUEST_TIMEOUT, TimeUnit.SECONDS)
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+            .build()
+            .let { client ->
+                Retrofit.Builder()
+                    .baseUrl(apiUrl)
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .build()
+            }
+    }
+}
+
+val dataModules = listOf(
+    networkModule
+)
