97c024b629042bec38555915bf7e5037632d1db6
Bruno Pimentel <bruno@pimentel.dev>
2020-04-16 16:41:43 -0300

    feat: creates usecase to get errortype
    - creates GetErrorType returning ErrorType enum
    - adds GetErrorType unit tests
    - changes BaseViewModel error handling to return a String resource
    instead a String
    - changes BaseViewModel tests for the new error handling
    - adds NullableFieldsViewModelTests for BaseViewModel with only nullable
    fields on constructor

--- domain/src/main/java/dev/pimentel/domain/TestClass.kt
+++
@@ -1,7 +0,0 @@
-package dev.pimentel.domain
-
-class TestClass {
-
-    @Suppress("FunctionOnlyReturningConstant")
-    fun returnTwo() = 2
-}
---
+++ domain/src/main/java/dev/pimentel/domain/usecases/GetErrorType.kt
@@ -0,0 +1,14 @@
+package dev.pimentel.domain.usecases
+
+import dev.pimentel.domain.entities.ErrorType
+import java.io.IOException
+
+class GetErrorType : UseCase<GetErrorTypeParams, ErrorType> {
+
+    override fun invoke(params: GetErrorTypeParams): ErrorType = when (params.throwable) {
+        is IOException -> ErrorType.NO_CONNECTION
+        else -> ErrorType.DEFAULT
+    }
+}
+
+data class GetErrorTypeParams(val throwable: Throwable)
