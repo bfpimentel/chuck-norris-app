6f8bbfa9819082decccc76492ebf02d5a592c7dd
Bruno Pimentel <bruno@pimentel.dev>
2020-04-17 20:38:43 -0300

    feat: adds viewmodel tests
    - adds facts viewmodel test for navigatior
    - adds search viewmodel test for getcategorysuggestions

--- app/src/test/java/dev/pimentel/chucknorris/presentation/facts/FactsViewModelTest.kt
+++ app/src/test/java/dev/pimentel/chucknorris/presentation/facts/FactsViewModelTest.kt
@@ -1,15 +1,31 @@
 package dev.pimentel.chucknorris.presentation.facts
 
+import dev.pimentel.chucknorris.R
+import dev.pimentel.chucknorris.shared.navigator.Navigator
 import dev.pimentel.chucknorris.testshared.ViewModelTest
-import org.junit.jupiter.api.Assertions.assertNotNull
+import io.mockk.*
+import org.junit.jupiter.api.Test
 
 class FactsViewModelTest : ViewModelTest<FactsContract.ViewModel>() {
 
+    private val navigator = mockk<Navigator>()
     override lateinit var viewModel: FactsContract.ViewModel
 
     override fun `setup subject`() {
-        viewModel = FactsViewModel(getErrorType, schedulerProvider)
+        viewModel = FactsViewModel(
+            navigator,
+            getErrorType,
+            schedulerProvider
+        )
+    }
 
-        assertNotNull(viewModel)
+    @Test
+    fun `should navigate to search`() {
+        every { navigator.navigate(R.id.search_fragment) } just runs
+
+        viewModel.navigateToSearch()
+
+        verify(exactly = 1) { navigator.navigate(R.id.search_fragment) }
+        confirmVerified(navigator, getErrorType)
     }
 }
