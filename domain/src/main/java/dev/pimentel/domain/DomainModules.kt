907f96b7f185f0a7d991b61e0efb9187a9314cc7
Bruno Pimentel <bruno@pimentel.dev>
2020-04-17 20:45:59 -0300

    fix: fixes detekt issues

--- domain/src/main/java/dev/pimentel/domain/DomainModules.kt
+++ domain/src/main/java/dev/pimentel/domain/DomainModules.kt
@@ -1,7 +1,12 @@
 package dev.pimentel.domain
 
 import dev.pimentel.data.dataModules
-import dev.pimentel.domain.usecases.*
+import dev.pimentel.domain.usecases.FetchAllCategories
+import dev.pimentel.domain.usecases.FetchAllCategoriesNames
+import dev.pimentel.domain.usecases.GetCategorySuggestions
+import dev.pimentel.domain.usecases.GetErrorType
+import dev.pimentel.domain.usecases.SaveAllCategories
+import dev.pimentel.domain.usecases.ShuffleList
 import org.koin.dsl.module
 
 private val useCasesModule = module {
