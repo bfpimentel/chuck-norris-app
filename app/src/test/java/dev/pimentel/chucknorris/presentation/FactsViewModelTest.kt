f312e4cbbf1642cfc7efcfc6d2104c6710a2a01d
Bruno Pimentel <bruno@pimentel.dev>
2020-04-19 18:49:48 -0300

    feat: adds unit tests for facts view model
    - adds unit tests for new methods on facts view model

--- app/src/test/java/dev/pimentel/chucknorris/presentation/FactsViewModelTest.kt
+++ app/src/test/java/dev/pimentel/chucknorris/presentation/FactsViewModelTest.kt
@@ -6,6 +6,7 @@ import dev.pimentel.chucknorris.presentation.facts.FactsViewModel
 import dev.pimentel.chucknorris.shared.navigator.Navigator
 import dev.pimentel.chucknorris.testshared.ViewModelTest
 import dev.pimentel.domain.entities.Fact
+import dev.pimentel.domain.usecases.GetErrorMessage
 import dev.pimentel.domain.usecases.GetFacts
 import dev.pimentel.domain.usecases.GetSearchTerm
 import dev.pimentel.domain.usecases.shared.NoParams
@@ -18,6 +19,7 @@ import io.mockk.verify
 import io.reactivex.Single
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Test
 
 class FactsViewModelTest : ViewModelTest<FactsContract.ViewModel>() {
@@ -44,11 +46,11 @@ class FactsViewModelTest : ViewModelTest<FactsContract.ViewModel>() {
         viewModel.navigateToSearch()
 
         verify(exactly = 1) { navigator.navigate(R.id.search_fragment) }
-        confirmVerified(navigator, getSearchTerm, getFacts)
+        confirmVerified(navigator, getSearchTerm, getFacts, getErrorMessage)
     }
 
     @Test
-    fun `should get facts and map them to facts displays`() {
+    fun `should get facts and map them to facts displays after getting them successfully`() {
         val term = "term"
         val getFactsParams = GetFacts.Params(term)
 
@@ -83,7 +85,51 @@ class FactsViewModelTest : ViewModelTest<FactsContract.ViewModel>() {
             getSearchTerm(NoParams)
             getFacts(getFactsParams)
         }
-        confirmVerified(navigator, getSearchTerm, getFacts)
+        confirmVerified(navigator, getSearchTerm, getFacts, getErrorMessage)
+    }
+
+    @Test
+    fun `should post value on error after failing to get facts`() {
+        val term = "term"
+        val getFactsParams = GetFacts.Params(term)
+        val error = IllegalArgumentException()
+        val getErrorMessageParams = GetErrorMessage.Params(error)
+        val errorMessage = "errorMessage"
+
+        every { getSearchTerm(NoParams) } returns Single.just(term)
+        every { getFacts(getFactsParams) } returns Single.error(error)
+        every { getErrorMessage(getErrorMessageParams) } returns errorMessage
+
+        assertNull(viewModel.error().value)
+
+        viewModel.setupFacts()
+        testScheduler.triggerActions()
+
+        assertEquals(viewModel.error().value, errorMessage)
+
+        verify(exactly = 1) {
+            getSearchTerm(NoParams)
+            getFacts(getFactsParams)
+            getErrorMessage(getErrorMessageParams)
+        }
+        confirmVerified(navigator, getSearchTerm, getFacts, getErrorMessage)
+    }
+
+    @Test
+    fun `should post value on first access after failing to get search term`() {
+        every { getSearchTerm(NoParams) } returns Single.error(GetSearchTerm.SearchTermNotFoundException())
+
+        assertNull(viewModel.firstAccess().value)
+
+        viewModel.setupFacts()
+        testScheduler.triggerActions()
+
+        assertNotNull(viewModel.firstAccess().value)
+
+        verify(exactly = 1) {
+            getSearchTerm(NoParams)
+        }
+        confirmVerified(navigator, getSearchTerm, getFacts, getErrorMessage)
     }
 
     @Test
