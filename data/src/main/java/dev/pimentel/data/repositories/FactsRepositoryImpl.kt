58c95f134ba0d0af494212a1a6acdc5d4c98f8ac
Bruno Pimentel <bruno@pimentel.dev>
2020-12-09 17:20:47 -0300

    fix: fix coroutine dependency and reference

--- data/src/main/java/dev/pimentel/data/repositories/FactsRepositoryImpl.kt
+++ data/src/main/java/dev/pimentel/data/repositories/FactsRepositoryImpl.kt
@@ -2,25 +2,22 @@ package dev.pimentel.data.repositories
 
 import dev.pimentel.data.sources.remote.FactsRemoteDataSource
 import dev.pimentel.domain.repositories.FactsRepository
-import io.reactivex.Single
 import dev.pimentel.domain.models.FactsResponse as DomainFactsResponse
 
 internal class FactsRepositoryImpl(
     private val remoteDataSource: FactsRemoteDataSource
 ) : FactsRepository {
 
-    override fun getFacts(searchTerm: String): Single<DomainFactsResponse> =
-        remoteDataSource.getFacts(searchTerm)
-            .map { response ->
-                DomainFactsResponse(
-                    response.result.map { fact ->
-                        DomainFactsResponse.Fact(
-                            fact.id,
-                            fact.categories,
-                            fact.url,
-                            fact.value
-                        )
-                    }
-                )
-            }
+    override suspend fun getFacts(searchTerm: String): DomainFactsResponse =
+        DomainFactsResponse(
+            result = remoteDataSource.getFacts(searchTerm).result
+                .map { fact ->
+                    DomainFactsResponse.Fact(
+                        fact.id,
+                        fact.categories,
+                        fact.url,
+                        fact.value
+                    )
+                }
+        )
 }
--- data/src/main/java/dev/pimentel/data/sources/remote/FactsRemoteDataSource.kt
+++ data/src/main/java/dev/pimentel/data/sources/remote/FactsRemoteDataSource.kt
@@ -1,12 +1,11 @@
 package dev.pimentel.data.sources.remote
 
 import dev.pimentel.data.models.FactsResponse
-import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface FactsRemoteDataSource {
 
     @GET("/jokes/search")
-    fun getFacts(@Query("query") searchTerm: String): Single<FactsResponse>
+    suspend fun getFacts(@Query("query") searchTerm: String): FactsResponse
 }
